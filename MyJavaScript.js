b41be32b75a7fa4766a857cd3188b1de3c570382
Azizi <munaemazizi@gmail.com>
2022-09-12 00:12:15 -0700

    Add file

---
+++ MyJavaScript.js
@@ -0,0 +1,64 @@
+
+// const input = document.querySelector('#rmsil');
+
+// function vaildate(field, regex){
+//     if(regex.test(field.value)){
+//         field.className = 'valid'
+//     }else{
+//         field.className = 'invalid'
+//     }
+// }
+
+// input.addEventListener('keyup', function(e){
+// // console.log(e.target.attributes.name.value)
+
+// vaildate(e.target, /^([a-zA-Z0-9\.-_]+)@([a-zA-Z0-9]{2,8})\.[a-zA-Z0-9]{2,8})(\.[a-zA-Z0-9]{2.8})?$/)
+// })
+// =========================================================================================================
+
+
+// function say(){
+//     return 'Hello'
+// }
+
+// console.log(say());
+
+// let myfunc = ()=>{
+//     console.log('hello from arrow function')
+// }
+
+// myfunc()
+
+
+
+// hello = ()=> 'Hi Dear'
+
+// console.log(hello('Mesbah'))
+
+// ==========================================================================================================
+// JavaScript Class:
+
+// class person{
+//     constructor(name, age){
+//         this.theName = name;
+//         this.theAge = age;
+//     }
+// }
+
+// let pobj = new person('Ahmad', 23)
+// let pobj2 = new person('israil', 30)
+
+// console.log(pobj.theName)
+// console.log(pobj.theAge)
+
+// console.log(pobj2.theName)
+// console.log(pobj2.theAge)
+
+// =======================================================================================
+
+
+
+
+
+
+
